951d077677f6cf21b2489dc3a00447971bcb854e
MoscosoMaximiliano <moscosomaximiliano@gmail.com>
2023-09-07 11:56:14 -0300

    fix route loading with hostname

--- js/script.js
+++ js/script.js
@@ -10,8 +10,9 @@ window.onload = () => {
 
         document.getElementById("createClassForm").addEventListener("submit", CreateNewClass)
     } else {
+        console.log(window.location.hostname);
         // TODO: Load data and enter the game
-        if(document.location.pathname === "http://127.0.0.1:5500")
+        if(document.location.host === "127.0.0.1:5500")
             window.location.href = "http://127.0.0.1:5500/mainGame.html"
         else
             window.location.href = "https://moscosomaximiliano.github.io/coderhouse-js-preentrega3-moscoso/mainGame.html"
